4aee0d8c995747e3d17e3da32b8a140010af1c78
KanarXD <piciolo@outlook.com>
2024-10-13 12:25:06 +0200

    parsing games works

--- 2023/task2/part1/src/main.rs
+++ 2023/task2/part1/src/main.rs
@@ -1,4 +1,30 @@
+use crate::Color::{BLUE, GREEN, RED};
+use std::cmp::max;
 use std::fs;
+
+struct GameTurn {
+    red: u32,
+    green: u32,
+    blue: u32,
+}
+
+#[derive(Debug)]
+struct Game {
+    id: u32,
+    max_red: u32,
+    max_green: u32,
+    max_blue: u32,
+}
+
+enum Color {
+    RED,
+    GREEN,
+    BLUE,
+}
+struct CubeMove {
+    value: u32,
+    color: Color,
+}
 
 fn main() {
     let file_path = "res/demo_input.txt";
@@ -16,17 +42,72 @@ fn main() {
 }
 
 fn process_data(data: String) -> Vec<u32> {
-    data.split("\n").map(|line| {
-        let tokens: Vec<&str> = line.split(" ").collect();
-
-        let id_token = tokens[1];
-        let id = &id_token[0..id_token.len() - 1];
-
-
-        return id;
+    let games: Vec<Game> = data.split("\n").map(|line| {
+        let mut parts = line.split(": ");
+        let first_part = parts.next()
+            .expect("line has to have first part");
+        let second_part = parts.next()
+            .expect("line has to have second part");
+        return (first_part, second_part);
     })
-        .map(|number| number.parse::<u32>().expect(format!("{number} is not a number").as_str()))
-        .collect()
+        .map(|(first_part, second_part)| {
+            let id: u32 = first_part.split(' ')
+                .nth(1)
+                .expect(format!("{first_part} has to have id").as_str())
+                .parse()
+                .expect(format!("{first_part} is not a number").as_str());
+            parse_game(id, second_part)
+        })
+        .collect();
+
+    println!("{:?}", games);
+
+    Vec::new()
+}
+
+fn parse_game(id: u32, games_string: &str) -> Game {
+    let game_turns_string = games_string.split("; ");
+    let mut game = Game { id, max_red: 0, max_green: 0, max_blue: 0 };
+    for game_turn_string in game_turns_string {
+        let GameTurn { red, green, blue } = parse_game_turn(game_turn_string);
+        game.max_red = max(game.max_red, red);
+        game.max_green = max(game.max_green, green);
+        game.max_blue = max(game.max_blue, blue);
+    }
+    game
+}
+
+fn parse_game_turn(game_turn_string: &str) -> GameTurn {
+    let game_moves_string = game_turn_string.split(", ");
+    let mut game_turn = GameTurn { red: 0, green: 0, blue: 0 };
+    for game_move_string in game_moves_string {
+        let CubeMove { color, value } = parse_cube_move_string(game_move_string);
+        match color {
+            RED => { game_turn.red = value }
+            GREEN => { game_turn.green = value }
+            BLUE => { game_turn.blue = value }
+        }
+    }
+    game_turn
+}
+
+fn parse_cube_move_string(cube_move_string: &str) -> CubeMove {
+    let mut cube_move_parts = cube_move_string.split(' ');
+
+    let value_string = cube_move_parts.next()
+        .expect("cube_move_parts has to have value part");
+    let value: u32 = value_string.parse()
+        .expect(format!("{value_string} has to be a number").as_str());
+
+    let color_string = cube_move_parts.next()
+        .expect("cube_move_parts has to have color part");
+    let color = match color_string {
+        "red" => RED,
+        "green" => GREEN,
+        "blue" => BLUE,
+        _ => { panic!("{color_string} has to be color") }
+    };
+    CubeMove { value, color }
 }
 
 
