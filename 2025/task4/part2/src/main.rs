9b63250af88867ed37f1de59f4c3319bce32b756
KanarXD <piciolo@outlook.com>
2025-12-04 19:32:09 +0100

    2025 t4p2 works

--- 2025/task4/part2/src/main.rs
+++ 2025/task4/part2/src/main.rs
@@ -1,39 +1,16 @@
-use std::collections::HashSet;
 use std::fmt::Debug;
 use std::fs;
-use std::hash::Hash;
 
+#[derive(Debug, Hash, PartialEq, Eq, Clone, Copy)]
+enum Tile {
+    Empty,
+    Paper,
+}
 #[derive(Debug, Hash, PartialEq, Eq, Clone, Copy)]
 struct Position {
-    y: isize,
-    x: isize,
+    x: usize,
+    y: usize,
 }
-
-impl Position {
-    fn x_coordinate_equals(&self, position: &Position) -> bool {
-        self.x == position.x
-    }
-
-    fn y_coordinate_equals(&self, position: &Position) -> bool {
-        self.y == position.y
-    }
-
-    fn x_difference(&self, position: &Position) -> isize {
-        self.x - position.x
-    }
-
-    fn y_difference(&self, position: &Position) -> isize {
-        self.y - position.y
-    }
-}
-
-#[derive(Debug, Hash, PartialEq, Eq, Clone, Copy)]
-struct Edge {
-    vertical: bool,
-    start: Position,
-    end: Position,
-}
-
 fn main() {
     let file_path = "res/demo_input.txt";
     let file_path = "res/input.txt";
@@ -42,223 +19,155 @@ fn main() {
         fs::read_to_string(file_path).expect(format!("failed to read file: {file_path}").as_str());
     // println!("{}", data);
 
-    let (locations, letters): (Vec<Vec<char>>, HashSet<char>) = parse_lines(data);
-    println!("locations: {:?}", locations);
-    println!("letters: {:?}", letters);
+    let matrix: Vec<Vec<Tile>> = parse_lines(data);
+    print_matrix(&matrix, None);
 
-    let sum = process_data(&locations, &letters);
+    let sum = process_data(&matrix);
     println!("sum={}", sum)
 }
 
-fn check_location(locations: &Vec<Vec<char>>, position: &Position) -> Option<char> {
-    if let Some(line) = locations.get(position.y as usize) {
-        if let Some(letter) = line.get(position.x as usize) {
-            return Some(*letter);
-        }
-    }
-    None
-}
-
-fn parse_lines(data: String) -> (Vec<Vec<char>>, HashSet<char>) {
-    let letters = data.chars().filter(|&l| l != '\n').collect();
-    let locations = data
-        .split("\n")
-        .map(|line| line.chars().collect())
-        .collect();
-    (locations, letters)
-}
-
-fn process_data(locations: &Vec<Vec<char>>, letters: &HashSet<char>) -> u64 {
+fn process_data(matrix: &Vec<Vec<Tile>>) -> u64 {
     let mut sum = 0;
-    let width = locations[0].len();
-    let height = locations.len();
-    for &letter in letters {
-        // if letter != 'E' {
-        //     continue;
-        // }
-        let mut checked_regions: HashSet<Position> = HashSet::new();
-        for y in 0..height {
-            for x in 0..width {
-                let checked_letter = locations[y][x];
-                let position = Position {
-                    x: x as isize,
-                    y: y as isize,
-                };
-                if checked_letter != letter || checked_regions.contains(&position) {
-                    continue;
-                }
-                let mut checked_regions_single: HashSet<Position> = HashSet::new();
-                let mut checked_edges: HashSet<Edge> = HashSet::new();
 
-                check_region(
-                    locations,
-                    letter,
-                    &mut checked_regions_single,
-                    &mut checked_edges,
-                    &position,
-                    position.clone(),
-                );
-                // let mut borders: u64 = checked_edges
-                //     .iter()
-                //     .map(|edge| bonus_borders(locations, letter, edge))
-                //     .sum::<u64>();
-                let mut borders: u64 = checked_edges.len() as u64;
+    let mut matrix_a: Vec<Vec<Tile>> = copy_matrix(matrix);
+    let mut matrix_b: Vec<Vec<Tile>> = copy_matrix(matrix);
 
-                let area = checked_regions_single.len() as u64;
-                let price = borders * area;
-                println!("letter={letter}, borders={borders}, area={area}, price={price}");
-                println!("edges={:?}", checked_edges);
-
-                sum += price;
-                checked_regions.extend(checked_regions_single);
-            }
+    loop {
+        let (result_matrix, matrix, result) = process_matrix(matrix_a, matrix_b);
+        matrix_a = result_matrix;
+        matrix_b = override_matrix(matrix, &matrix_a);
+        sum += result;
+        if result == 0 {
+            break;
         }
     }
+
     sum
 }
 
-fn insert_edge(
-    locations: &Vec<Vec<char>>,
-    letter: char,
-    mut checked_edges: &mut HashSet<Edge>,
-    position: &Position,
-    mut edge: Edge,
-) {
-    let width = locations[0].len() as isize;
-    let height = locations.len() as isize;
-    if edge.vertical {
-        for x in position.x + 1..=width {
-            edge.start.x = x - 1;
-            edge.end.x = x - 1;
-            // let checked_letter = locations[edge.start.y as usize][x as usize];
-            if let Some(checked_letter) =
-                check_location(locations, &Position { y: edge.start.y, x })
-            {
-                if checked_letter == letter {
-                    if let Some(end_letter) =
-                        check_location(locations, &Position { y: edge.end.y, x })
-                    {
-                        if end_letter == letter {
-                            break;
-                        }
-                    }
-                } else {
-                    break;
-                }
-            }
-        }
-    } else {
-        for y in position.y + 1..=height {
-            // let checked_letter = locations[y as usize][edge.start.x as usize];
-            edge.start.y = y - 1;
-            edge.end.y = y - 1;
-            if let Some(checked_letter) =
-                check_location(locations, &Position { y, x: edge.start.x })
-            {
-                if checked_letter == letter {
-                    if let Some(end_letter) =
-                        check_location(locations, &Position { y, x: edge.end.x })
-                    {
-                        if end_letter == letter {
-                            break;
-                        }
-                    }
-                } else {
-                    break;
-                }
+fn process_matrix(
+    matrix: Vec<Vec<Tile>>,
+    mut result_matrix: Vec<Vec<Tile>>,
+) -> (Vec<Vec<Tile>>, Vec<Vec<Tile>>, u64) {
+    let mut sum = 0;
+
+    let mut positions = vec![];
+
+    for y in 0..matrix.len() {
+        for x in 0..matrix[0].len() {
+            if check_tile(&matrix, x as isize, y as isize) {
+                sum += 1;
+                result_matrix[y][x] = Tile::Empty;
+                positions.push(Position { x, y });
             }
         }
     }
-    checked_edges.insert(edge);
-    // panic!("failed to insert edge");
+
+    print_matrix(&matrix, Some(&positions));
+
+    (result_matrix, matrix, sum)
 }
 
-fn check_region(
-    locations: &Vec<Vec<char>>,
-    letter: char,
-    mut checked_regions: &mut HashSet<Position>,
-    mut checked_edges: &mut HashSet<Edge>,
-    previous_position: &Position,
-    position: Position,
-) {
-    // println!("position={position:?}");
-    if let Some(region_letter) = check_location(locations, &position) {
-        if checked_regions.contains(&position) {
-            return;
+fn override_matrix(mut target_matrix: Vec<Vec<Tile>>, matrix: &Vec<Vec<Tile>>) -> Vec<Vec<Tile>> {
+    for y in 0..matrix.len() {
+        for x in 0..matrix[0].len() {
+            target_matrix[y][x] = matrix[y][x];
         }
-        if region_letter != letter {
-            let edge = create_edge(previous_position, &position);
-            insert_edge(locations, letter, checked_edges, &position, edge);
-            return;
-        }
-
-        checked_regions.insert(position);
-
-        check_region(
-            locations,
-            letter,
-            &mut checked_regions,
-            &mut checked_edges,
-            &position,
-            Position {
-                x: position.x - 1,
-                y: position.y,
-            },
-        );
-        check_region(
-            locations,
-            letter,
-            &mut checked_regions,
-            &mut checked_edges,
-            &position,
-            Position {
-                x: position.x,
-                y: position.y - 1,
-            },
-        );
-        check_region(
-            locations,
-            letter,
-            &mut checked_regions,
-            &mut checked_edges,
-            &position,
-            Position {
-                x: position.x + 1,
-                y: position.y,
-            },
-        );
-        check_region(
-            locations,
-            letter,
-            &mut checked_regions,
-            &mut checked_edges,
-            &position,
-            Position {
-                x: position.x,
-                y: position.y + 1,
-            },
-        );
-    } else {
-        let edge = create_edge(previous_position, &position);
-        insert_edge(locations, letter, checked_edges, &position, edge);
     }
+    target_matrix
 }
 
-fn create_edge(previous_position: &Position, position: &Position) -> Edge {
-    if previous_position.y_coordinate_equals(&position) {
-        let edge = Edge {
-            vertical: false,
-            start: *previous_position,
-            end: *position,
-        };
-        return edge;
-    } else if previous_position.x_coordinate_equals(&position) {
-        let edge = Edge {
-            vertical: true,
-            start: *previous_position,
-            end: *position,
-        };
-        return edge;
+fn copy_matrix(matrix: &Vec<Vec<Tile>>) -> Vec<Vec<Tile>> {
+    let mut result_matrix = vec![];
+    for row in matrix {
+        let mut result_row = vec![];
+        for tile in row {
+            result_row.push(tile.clone());
+        }
+        result_matrix.push(result_row);
     }
-    panic!("Something went wrong");
+    result_matrix
+}
+
+fn check_tile(matrix: &Vec<Vec<Tile>>, x: isize, y: isize) -> bool {
+    if is_tile_equal_to(matrix, x, y, &Tile::Empty) {
+        return false;
+    }
+
+    let to_check = vec![
+        (x - 1, y),
+        (x - 1, y - 1),
+        (x - 1, y + 1),
+        (x + 1, y),
+        (x + 1, y - 1),
+        (x + 1, y + 1),
+        (x, y - 1),
+        (x, y + 1),
+    ];
+
+    let paper_neighbours: u32 = to_check
+        .iter()
+        .map(|(tx, ty)| {
+            if is_tile_equal_to(matrix, *tx, *ty, &Tile::Paper) {
+                return 1;
+            }
+            return 0;
+        })
+        .sum();
+
+    // println!("x={} y={} paper_neighbours={}", x, y, paper_neighbours);
+
+    paper_neighbours < 4
+}
+
+fn is_tile_equal_to(matrix: &Vec<Vec<Tile>>, x: isize, y: isize, tile: &Tile) -> bool {
+    if y < 0 || y >= matrix.len() as isize || x < 0 || x >= matrix[0].len() as isize {
+        return false;
+    }
+    matrix[y as usize][x as usize] == *tile
+}
+
+fn parse_lines(data: String) -> Vec<Vec<Tile>> {
+    data.split("\n")
+        .map(|line| {
+            line.chars()
+                .map(|c| {
+                    return match c {
+                        '.' => Tile::Empty,
+                        '@' => Tile::Paper,
+                        _ => panic!("bad input"),
+                    };
+                })
+                .collect()
+        })
+        .collect()
+}
+
+fn print_matrix(matrix: &Vec<Vec<Tile>>, positions: Option<&Vec<Position>>) {
+    println!("\nmatrix:");
+    let mut result = vec![];
+    for row in matrix {
+        let mut row_result = vec![];
+        for tile in row {
+            let tile = match tile {
+                Tile::Empty => '.',
+                Tile::Paper => '@',
+            };
+            row_result.push(tile);
+        }
+        result.push(row_result);
+    }
+
+    if let Some(positions) = positions {
+        for &Position { x, y } in positions.iter() {
+            result[y][x] = 'x';
+        }
+    }
+
+    for row in result {
+        for tile in row {
+            print!("{}", tile);
+        }
+        println!();
+    }
 }
