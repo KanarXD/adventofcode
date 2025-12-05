0ce1aa30a7dfb8893c6e2b3028c6ad2c2023b82b
KanarXD <piciolo@outlook.com>
2025-12-05 21:52:56 +0100

    2025 t5p2 works

--- 2025/task5/part2/src/main.rs
+++ 2025/task5/part2/src/main.rs
@@ -1,3 +1,4 @@
+use std::collections::VecDeque;
 use std::fmt::Debug;
 use std::fs;
 
@@ -11,6 +12,20 @@ impl Range {
     fn contains(&self, x: u64) -> bool {
         x >= self.left && x <= self.right
     }
+    fn overlaps(&self, range: &Range) -> bool {
+        self.contains(range.left)
+            || self.contains(range.right)
+            || range.contains(self.left)
+            || range.contains(self.right)
+    }
+    fn add_range(&self, range: &Range) -> Self {
+        let left = self.left.min(range.left);
+        let right = self.right.max(range.right);
+        Range { left, right }
+    }
+    fn points(&self) -> u64 {
+        self.right - self.left + 1
+    }
 }
 
 fn main() {
@@ -22,30 +37,50 @@ fn main() {
     // println!("{}", data);
 
     let (ranges, ids) = parse_lines(data);
-    println!("ranges = {:?}", ranges);
+    println!("ranges={:?}", ranges);
     println!("ids = {:?}", ids);
 
-    let sum = process_data(&ranges, &ids);
+    let sum = process_data(ranges);
     println!("sum={}", sum)
 }
 
-fn process_data(ranges: &Vec<Range>, ids: &Vec<u64>) -> u64 {
-    let mut sum = 0;
+fn process_data(mut ranges: VecDeque<Range>) -> u64 {
+    loop {
+        let last_size = ranges.len();
+        for i in 0..last_size {
+            let base_range = ranges.pop_front().unwrap();
+            ranges = check_range(base_range, ranges);
+            println!("i={i} ranges={ranges:?}");
+        }
 
-    for &id in ids {
-        'ranges: for range in ranges {
-            if range.contains(id) {
-                sum += 1;
-                println!("range contains {}", id);
-                break 'ranges;
-            }
+        if ranges.len() >= last_size {
+            break;
         }
     }
 
-    sum
+    ranges.iter().map(|r| r.points()).sum::<u64>()
 }
 
-fn parse_lines(data: String) -> (Vec<Range>, Vec<u64>) {
+fn check_range(mut base_range: Range, ranges: VecDeque<Range>) -> VecDeque<Range> {
+    let mut result: VecDeque<Range> = ranges
+        .into_iter()
+        .map(|range| {
+            if base_range.overlaps(&range) {
+                base_range = base_range.add_range(&range);
+                None
+            } else {
+                Some(range)
+            }
+        })
+        .flatten()
+        .collect();
+
+    result.push_back(base_range);
+
+    result
+}
+
+fn parse_lines(data: String) -> (VecDeque<Range>, Vec<u64>) {
     let parts: Vec<&str> = data.split("\n\n").collect();
     let ranges = *parts.get(0).unwrap();
     let ids = *parts.get(1).unwrap();
